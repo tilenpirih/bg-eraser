876e8c8838b0684c842ba301e2026095d8275048
Tilen Pirih <tilen.pirih@gmail.com>
2025-01-04 16:53:23 +0100

    Fix metatags

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-const runtimeConfig = useRuntimeConfig()
-useHead({
-  title: 'Free image background remover',
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { name: 'description', content: 'Remove image backgrounds instantly and for free. No limits, no hidden fees. Just fast and easy background remover.' },
-    { property: 'og:title', content: 'Free image background remover' },
-    { property: 'og:image', content: `${runtimeConfig.public.BASE_URL}/img/OGImage.png` },
-    { property: 'og:image:width', content: '1200' },
-    { property: 'og:image:height', content: '630' },
-    { property: 'og:type', content: 'website' },
-    { property: 'og:url', content: `${runtimeConfig.public.BASE_URL}` },
-    { name: 'keywords', content: 'background, remover, free, background remover, image, image background remover, remove background, remove image background' },
-    { name: 'author', content: 'Tilen Pirih' },
-  ],
-})
+// const runtimeConfig = useRuntimeConfig()
+// useHead({
+//   title: 'Free image background remover',
+//   meta: [
+//     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+//     { name: 'description', content: 'Remove image backgrounds instantly and for free. No limits, no hidden fees. Just fast and easy background remover.' },
+//     { property: 'og:title', content: 'Free image background remover' },
+//     { property: 'og:image', content: `${runtimeConfig.public.BASE_URL}/img/OGImage.png` },
+//     { property: 'og:image:width', content: '1200' },
+//     { property: 'og:image:height', content: '630' },
+//     { property: 'og:type', content: 'website' },
+//     { property: 'og:url', content: `${runtimeConfig.public.BASE_URL}` },
+//     { name: 'keywords', content: 'background, remover, free, background remover, image, image background remover, remove background, remove image background' },
+//     { name: 'author', content: 'Tilen Pirih' },
+//   ],
+// })
 </script>
 
 <template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,23 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   ssr: false,
   devtools: { enabled: false },
+  app:{
+    head:{
+      title: 'Free image background remover',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Remove image backgrounds instantly and for free. No limits, no hidden fees. Just fast and easy background remover.' },
+        { property: 'og:title', content: 'Free image background remover' },
+        { property: 'og:image', content: `${process.env.BASE_URL}/img/OGImage.png` },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '630' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: `${process.env.BASE_URL}` },
+        { name: 'keywords', content: 'background, remover, free, background remover, image, image background remover, remove background, remove image background' },
+        { name: 'author', content: 'Tilen Pirih' },
+      ],
+    }
+  },
   runtimeConfig: {
     public: {
       BASE_URL: process.env.BASE_URL,
@@ -30,9 +47,6 @@ export default defineNuxtConfig({
         transformAssetUrls,
       },
     },
-  },
-  routeRules: {
-    '/**': { ssr: false },
   },
   nitro: {
     preset: 'bun',
